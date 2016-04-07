e998a34fc2a4fb23b3937f41f27a7e090a234e54
Ziyad.Jappie <ziyad.jappie@bsg.co.za>
2016-04-07 08:49:45 +0200

    Read excel files and add to list

--- src/main/java/za/co/discovery/dataAccess/readExcel.java
+++ src/main/java/za/co/discovery/dataAccess/readExcel.java
@@ -11,27 +11,38 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 public class readExcel {
+
+    //    @Autowired
     private VertexDAO vertexDAO;
 
-    public readExcel() {
-        try {
+    private final int numberOfVertices = 37;
+    List<Vertex> vertexList = new Vector<>(numberOfVertices);
 
+    public List<Vertex> readExcel() {
+
+        try {
+//            ClassLoader classLoader = this.getClass().getClassLoader();
+//            File fileEx = new File(classLoader.getResource("PlanetData.xlsx").getFile());
+//            FileInputStream file = new FileInputStream(fileEx);
             FileInputStream file = new FileInputStream(new File("C:\\PlanetData.xlsx"));
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheet("Planet Names");
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
+            int rowStart = Math.min(1, 1); // 0 based not 1 based rows
+            int rowEnd = Math.max(12, sheet.getLastRowNum());
+            for (int rowNum = rowStart; rowNum < rowEnd + 1; rowNum++) {
+                Row row = sheet.getRow(rowNum);
                 Iterator<Cell> cellIterator = row.cellIterator();
                 while (cellIterator.hasNext()) {
-//                    Cell cell = cellIterator.next();
                     String planetNode = row.getCell(0).getStringCellValue();
                     String planetName = row.getCell(1).getStringCellValue();
 //                    System.out.print(planetNode + " " + planetName + "\n\n");
                     Vertex vertex = new Vertex(planetNode);
                     vertex.setPlanetName(planetName);
+                    vertexList.add(vertex);
                     vertexDAO.save(vertex);
                     break;
                 }
@@ -44,6 +55,7 @@ public class readExcel {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return vertexList;
     }
 
 }
--- src/main/java/za/co/discovery/dataAccess/readExcel2.java
+++ src/main/java/za/co/discovery/dataAccess/readExcel2.java
@@ -4,54 +4,49 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import za.co.discovery.models.Edge;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Ziyad.Jappie on 2016/04/06.
  */
 public class readExcel2 {
+    @Autowired
     private EdgeDAO edgeDAO;
 
-    public readExcel2() {
+    private final int numberOfEdges = 45;
+    List<Edge> edgeList = new ArrayList<>(numberOfEdges);
+
+    public List<Edge> readExcel2() {
         try {
 
             FileInputStream file = new FileInputStream(new File("C:\\PlanetData.xlsx"));
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheet("Routes");
-            Iterator<Row> rowIterator = sheet.iterator();
             int rowStart = Math.min(1, 1); // 0 based not 1 based rows
             int rowEnd = Math.max(12, sheet.getLastRowNum());
-            for (int rowNum = rowStart; rowNum < rowEnd; rowNum++) {
+            for (int rowNum = rowStart; rowNum < rowEnd + 1; rowNum++) {
                 Row r = sheet.getRow(rowNum);
                 Iterator<Cell> cellIterator = r.cellIterator();
                 while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
                     Double routeId = r.getCell(0).getNumericCellValue();
                     String planetSource = r.getCell(1).getStringCellValue();
                     String planetDestination = r.getCell(2).getStringCellValue();
                     Double planetDistance = r.getCell(3).getNumericCellValue();
                     int routeId2 = Integer.valueOf(routeId.intValue());
                     Edge edge = new Edge(routeId2, planetSource, planetDestination, planetDistance);
+                    edgeList.add(edge);
                     edgeDAO.save(edge);
 //                    System.out.print(routeId + " " + planetSource + "\n\n");
                     break;
-//                    switch(cell.getCellType()) {
-//                        case Cell.CELL_TYPE_BOOLEAN:
-//                            System.out.print(cell.getBooleanCellValue() + "\t\t");
-//                            break;
-//                        case Cell.CELL_TYPE_NUMERIC:
-//                            System.out.print(cell.getNumericCellValue() + "\t\t");
-//                            break;
-//                        case Cell.CELL_TYPE_STRING:
-//                            System.out.print(cell.getStringCellValue() + "\t\t");
-//                            break;
-//                    }
                 }
                 System.out.println("");
             }
@@ -62,5 +57,6 @@ public class readExcel2 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return edgeList;
     }
 }
