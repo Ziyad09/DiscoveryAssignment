d1cca7af3ad78fe402308f87f56e7d37be26263a
Ziyad.Jappie <ziyad.jappie@bsg.co.za>
2016-04-13 12:47:30 +0200

    Implement SOAP

--- src/main/java/za/co/discovery/VertexEndpoint.java
+++ src/main/java/za/co/discovery/VertexEndpoint.java
@@ -1,26 +1,33 @@
 package za.co.discovery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import za.co.discovery.assignment.GetPathRequest;
+import za.co.discovery.assignment.GetPathResponse;
 
 //import io.spring.guides.gs_producing_web_service.GetCountryRequest;
 //import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 
 @Endpoint
 public class VertexEndpoint {
-//    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
-//    private VertexRepository vertexRepository;
-//
-//    @Autowired
-//    public VertexEndpoint(VertexRepository vertexRepository){
-//        this.vertexRepository = vertexRepository;
-//    }
-//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPathRequest")
-//
-//    @ResponsePayload
-//    public GetPathResponse getPathResponse(@RequestPayload GetPathRequest request){
-//        GetPathResponse response = new GetPathResponse();
-//        response.setPath(vertexRepository.findVertex(request.getName()));
-//
-//        return response;
-//    }
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+    private VertexRepository vertexRepository;
+
+    @Autowired
+    public VertexEndpoint(VertexRepository vertexRepository) {
+        this.vertexRepository = vertexRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPathRequest")
+
+    @ResponsePayload
+    public GetPathResponse getPathResponse(@RequestPayload GetPathRequest request) {
+        GetPathResponse response = new GetPathResponse();
+        response.setPath(vertexRepository.findVertex(request.getName()));
+
+        return response;
+    }
 }
