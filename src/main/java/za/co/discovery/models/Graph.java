d9beff098f511b2408aa18620ec4d03b8348cd1d
Ziyad.Jappie <ziyad.jappie@bsg.co.za>
2016-04-26 09:06:48 +0200

    Refactor code

--- src/main/java/za/co/discovery/models/Graph.java
+++ src/main/java/za/co/discovery/models/Graph.java
@@ -1,8 +1,5 @@
 package za.co.discovery.models;
 
-import com.sun.xml.internal.ws.developer.Serialization;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
